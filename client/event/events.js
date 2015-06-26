cf25c694cef771434b11f68b7691d9974bd86372
albertbahia <albert.bahia1@gmail.com>
2015-06-25 21:41:31 -0400

    create event page completed along with routing and event listeners for create event form submission.  installed velocity test framework packages that uses Jasmine syntax

---
+++ client/event/events.js
@@ -0,0 +1,36 @@
+Template.events.events({
+	"click #create-event" : function(event) {
+
+		var formEventData = document.getElementsByClassName("new-event");
+
+		Events.insert({
+			eventName: formEventData[0][0].value,
+			eventStreetAddress: formEventData[0][1].value,
+			eventNumberInAttendance: null,
+			eventPrice: formEventData[0][2].value,
+			eventVenueName: formEventData[0][3].value,
+			eventDuration: formEventData[0][4].value,
+			eventType: formEventData[0][5].value,
+			eventCalendarDate: formEventData[0][6].value,
+			eventCreatedAt: new Date(), //---Current time
+			eventHostName: formEventData[0][7].value,
+			eventNumberMalesAttending: null,
+			eventNumberFemalesAttending: null,
+			eventCity: formEventData[0][9].value,
+			eventState: formEventData[0][10].value,
+			eventZipCode: formEventData[0][11].value,
+			eventCreatorId: Meteor.userId(),
+			eventPublic: formEventData[0][12].value,
+			eventPrivate: formEventData[0][13].value,
+			eventVenueId: venue._id //---CHECK
+		});
+
+		// ---Clear form input fields
+		for (var i = 0; i < formEventData[0].length; i++) {
+			formEventData[0][i].value = "";
+		};
+
+		// ---Prevent default form submit
+		return false;
+	}
+});
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,148 +1,148 @@
 // Fixture data
 
 if (Events.find().count() === 0) {
-	
+
 	var now = new Date().getTime();
-	
+
 	Events.insert({
 		eventName: "Happy Hour",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreeteventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		eventDateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 5
 	})
-	
+
 		Events.insert({
 		eventName: "Happy Hour 2",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05",
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 10
 	})
-	
+
 		Events.insert({
 		eventName: "Happy Hour 3",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 15
 	})
-	
+
 		Events.insert({
 		eventName: "Happy Hour 4",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 20
 	})
 
 		Events.insert({
 		eventName: "Happy Hour 5",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 25
 	})
 
 		Events.insert({
 		eventName: "Happy Hour 6",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 30
 	})
 
 		Events.insert({
 		eventName: "Happy Hour 7",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 35
 	})
 
 		Events.insert({
 		eventName: "Happy Hour 8",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 40
 	})
 
 		Events.insert({
 		eventName: "Happy Hour 9",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 45
 	})
 
 		Events.insert({
 		eventName: "Happy Hour 10",
-		venueName: "Grasshopper",
-		address: "123 Main Street, Morristown NJ 12345",
-		startDate: "May 05", 
-		endDate: "May 05",
-		startTime: "1:00pm",
-		endTime: "4:00pm", 
+		eventVenueName: "Grasshopper",
+		eventStreetAddress: "123 Main Street, Morristown NJ 12345",
+		eventStartDate: "May 05",
+		eventEndDate: "May 05",
+		eventStartTime: "1:00pm",
+		eventEndTime: "4:00pm",
 		createdUser: "Joey",
-		dateCreated: new Date(now - 5*3600*1000), 
+		dateCreated: new Date(now - 5*3600*1000),
 		summary: "Happy Hour at the Grasshoppper!!! 1:00pm to 4:00pm",
 		peopleGoing: 50
-	})	
-	
-	
+	})
+
+
 }
