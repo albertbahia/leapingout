6309cfc3bb6048e37817dc473faf6414a0d71c06
albertbahia <albert.bahia1@gmail.com>
2015-10-10 01:12:52 -0400

    added email verification test

---
+++ server/server.js
@@ -0,0 +1,10 @@
+process.env.MAIL_URL="smtp://albert.bahia1%40gmail.com:JIGGAnick217@smtp.gmail.com:465";
+
+// ---Tests out email verification feature---
+// Email.send({
+//   from: "verification@leapingout.com",
+//   to: "albert.bahia1@gmail.com",
+//   subject: "Please verify email.",
+//   text: "Its pretty easy."
+// });
+// ------------------------------------------
