a84d245157fe8980b943053abc2049d5abca4924
albertbahia <albert.bahia1@gmail.com>
2015-10-10 00:30:03 -0400

    removed email verfication files since they were giving errors

--- server/smtp.js
+++
@@ -1,9 +0,0 @@
-Meteor.startup(function() {
-	smtp = {
-		username: 'info@leapingout.com',
-		password: 'test123',
-		server: 'smtp@gmail.com',
-		port: 25
-	}
-	process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
-});
