2f28301d165d21d13cd9abe1774be6366bd615d5
albertbahia <albert.bahia1@gmail.com>
2015-10-12 16:42:28 -0400

    added some attributes to the edit profile page and created an event listener for clicking the edit profile button

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -1,21 +1,48 @@
+//---Subscribes to the user collection publication---
 Meteor.subscribe("userData");
+// --------------------------------------------------
 
+// ---Handles main profile page---
+// ---Add anything here related to the main profile page---
 Template.profile.helpers({
     userEmail: function() {
         //console.log(Meteor.user().emails[0]);
         return Meteor.user().emails[0].address;
     },
     userFirstName: function() {
-        console.log(Meteor.user());
+        // console.log(Meteor.user());
         return Meteor.user().profile.userFirstName;
     },
     userLastName: function() {
         return Meteor.user().profile.userLastName;
     }
 });
+// -------------------------------
 
+// ---Handles edit profile page---
+// ---Add anything here related to the edit profile page---
 Template.editProfile.helpers({
     userEmail: function() {
+      console.log(Meteor.user());
         return Meteor.user().emails[0].address;
     }
 });
+
+Template.editProfile.events({
+  "click #edit-profile-button": function(event) {
+    event.preventDefault();
+
+    var userProfileObject = {};
+    // ----Corresponds with Profile Page and ERD for User---
+    var userProfileDataFields = ['userEmail', 'userFirstNAme', 'userLastName', 'userGender', 'userAge', 'userState', 'userZipCode'];
+    // -----------------------------------------------------
+    var userProfileDataArray = $(".profile-content-edit").children().find('input');
+    console.log(userProfileDataArray);
+    console.log(userProfileDataArray.length);
+
+    var currentUserId = Meteor.userId();
+
+    // Meteor.users.update(currentUserId,  )
+  }
+});
+// -------------------------------
