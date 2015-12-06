54804b78a4478c6f53c3e0101f985b2a96b9cffc
albertbahia <albert.bahia1@gmail.com>
2015-12-06 16:26:28 -0500

    turned autopublish off and enabled publish/subscribe for events

--- client/hotspot/hotspots.js
+++ client/hotspot/hotspots.js
@@ -1,14 +1,16 @@
+Meteor.subscribe("events");
+
 Template.hotspots.onRendered(function() {
     this.$('#InputDate').pickadate();
 	this.$('#InputTime').pickatime();
 });
 
 Template.hotspots.helpers({
-	
+
 	findEvent: function(){
-		
-		 return Events.find({}, {sort: {eventNumberInAttendance:-1}, limit:5});
-		
+		// console.log(Events.find({}, {sort: {eventNumberInAttendance:-1}, limit:5}));
+		 return Events.find({});
+
 	}
-	
+
 });
--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,12 @@
-Meteor.publish('images', function(limit) {
+Meteor.publish("images", function(limit) {
   check(limit, Number);
 
   return Images.find({}, {
    /*  limit: limit
 	sort: {uploadedAt:-1} */
   });
+});
+
+Meteor.publish("events", function() {
+	return Events.find({}, {sort: {eventNumberInAttendance:-1}, limit:5});
 });
