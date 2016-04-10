5f83052b4aafb0f32ae98146739f39d9924c3726
albertbahia <albert.bahia1@gmail.com>
2016-04-10 12:35:31 -0400

    implemented search-source package for events/venues search

--- client/channelGuide/channelGuide.js
+++ client/channelGuide/channelGuide.js
@@ -4,25 +4,11 @@ Meteor.subscribe("Tasks");
 Template.channelGuide.onRendered(function() {
     this.$('#InputDate').pickadate();
 	this.$('#InputTime').pickatime();
-
-
-
-
-
 });
 
 Template.channelGuide.helpers({
-
 	findEvent: function(){
 		// console.log(Events.find({}, {sort: {eventNumberInAttendance:-1}, limit:5}));
 		 return Events.find({});
-
 	}
-
-
-	
-
-
-
-
 });
---
+++ client/search/searchResults.js
@@ -0,0 +1,20 @@
+Template.searchResults.helpers({
+  getEvents: function() {
+    return EventSearch.getData({
+      transform: function(matchText, regExp) {
+        return matchText.replace(regExp, "<b>$&</b>")
+      },
+      sort: {eventNumberInAttendance: -1}
+    });
+  },
+  isLoading: function() {
+    return EventSearch.getStatus().loading;
+  }
+});
+
+Template.searchBox.events({
+  "keyup #search": _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    EventSearch.search(text);
+  }, 200)
+});
