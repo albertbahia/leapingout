a5a360ced2c7ddf2954f155f0f9e229d8ce3f050
albertbahia <albert.bahia1@gmail.com>
2016-08-14 15:34:50 -0400

    integrated new code from lepozepo into editProfile template

--- client/profile/profile.js
+++ client/profile/profile.js
@@ -26,54 +26,56 @@ Template.editProfile.helpers({
     },
     userProfileImage: function() {
       return Meteor.user().profile.image_url;
+    },
+    "files": function() {
+      return S3.collection.find();
     }
 });
 
 Template.editProfile.events({
-    'change #file-input': function(e) {
-        var user = Meteor.user();
-        FS.Utility.eachFile(e, function(file) {
-            var newFile = new FS.File(file);
+    // 'change #file-input': function(e) {
+    //     var user = Meteor.user();
+    //     FS.Utility.eachFile(e, function(file) {
+    //         var newFile = new FS.File(file);
+    //
+    //         newFile.username = user.username;
+    //         newFile.userId = user._id;
+    //         console.log(newFile);
+    //         Images.insert(newFile, function(error, fileObj) {
+    //             if (error) {
+    //                 toastr.error("Upload failed... please try again.");
+    //             } else {
+    //                 toastr.success('Upload succeeded!');
+    //                 console.log(fileObj);
+    //                 console.log(Meteor.settings);
+    //             }
+    //         });
+    //     });
+    // }
+    "click button.upload": function() {
+      var files = $("input.file_bag")[0].files
 
-            newFile.username = user.username;
-            newFile.userId = user._id;
-            console.log(newFile);
-            Images.insert(newFile, function(error, fileObj) {
-                if (error) {
-                    toastr.error("Upload failed... please try again.");
-                } else {
-                    toastr.success('Upload succeeded!');
-                    console.log(fileObj);
-                    console.log(Meteor.settings);
-                }
-            });
-        });
+      S3.upload({
+        files: files,
+        path: "Images" //subfolder for the leaping S3 bucket
+      }, function(error,response) {
+        if (error) {
+          throw error;
+        } else {
+          console.log(response.secure_url);
+          console.log(Meteor.user().profile.image_url);
+          Meteor.users.update(Meteor.userId(), {$set: {"profile.image_url": response.secure_url}})
+        }
+
+      })
     }
 });
 
-Template.s3_tester.events({
-  "click button.upload": function() {
-    var files = $("input.file_bag")[0].files
-
-    S3.upload({
-      files: files,
-      path: "Images" //subfolder for the leaping S3 bucket
-    }, function(error,response) {
-      if (error) {
-        throw error;
-      } else {
-        console.log(response.secure_url);
-        console.log(Meteor.user().profile.image_url);
-        Meteor.users.update(Meteor.userId(), {$set: {"profile.image_url": response.secure_url}})
-      }
-
-    })
-  }
-});
-
-Template.s3_tester.helpers({
-  "files": function() {
-    return S3.collection.find();
-  }
-});
+// Template.s3_tester.events({
+//
+// });
+//
+// Template.s3_tester.helpers({
+//
+// });
 // ----------------------------
