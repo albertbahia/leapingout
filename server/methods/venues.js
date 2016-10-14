66ca8dfbb6d28ed96cdc543d36c637abc1f0f5ae
albertbahia <albert.bahia1@gmail.com>
2016-10-13 21:43:17 -0400

    fixed meteor server side call

--- server/methods/venues.js
+++ server/methods/venues.js
@@ -1,11 +1,12 @@
 Meteor.methods({
-	createVenue: function(venue, user) {
+	createVenue: function(venue) {
 		check(venue, Object);
-		check(user, Object);
-		
+		//check(user, Object);
+
+		console.log("server method for creating venue");		
 		console.log(venue);
 		
 		//-save venue data to MongoDB here-
-
+		return "hello";
 	}
 });
