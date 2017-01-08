3503b4a3f9bdccf3cacddeb8799c5c163bf809bf
Sharpice17 <sharpice@gmail.com>
2017-01-08 12:53:32 -0500

    Faker update, Channel Guide updated

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,7 @@
 // Fixture data
 
 import _ from 'lodash';
-import { image, helpers, company, address, lorem } from 'faker';
+import { image, helpers, company, address, lorem, phone } from 'faker';
 
 Meteor.startup(function(){
 if (Meteor.users.find({}).count() === 0){
@@ -38,6 +38,9 @@ if (Venues.find({}).count() === 0) {
 			 const city = address.city();
 			 const stateAbbr = address.stateAbbr();
 			 const zipCode = address.zipCode();
+
+			 const phoneNumber = phone.phoneNumber();
+
 			 const latitude = address.latitude();
 			 const longitude = address.longitude();
 
@@ -52,6 +55,8 @@ if (Venues.find({}).count() === 0) {
 				 venueCity: city,
 				 venueStateAbbr: stateAbbr,
 				 venueZipCode: zipCode,
+
+				 venuePhone: phoneNumber,
 
 				 venueLat: latitude,
 				 venueLon: longitude,
@@ -97,6 +102,9 @@ if (Events.find({}).count() === 0) {
 
 	var now = Date.now();
 
+	min=moment().valueOf();
+	max=moment(min).add(30, 'days').valueOf();
+
 	 _.times(5000,function(){
 	 			//const companyName = company.companyName();
 
@@ -111,16 +119,26 @@ if (Events.find({}).count() === 0) {
 				const words = lorem.words();
 				const paragraph = lorem.paragraph();
 
-			//	var r = Math.floor(Math.random() * 5000);
-			//	var randomUser = Meteor.users.find().limit(1).skip(r);
+				var r = Math.floor(Math.random() * 5000);
+				var randomUserId = Meteor.users.find({}, {skip:r, limit:1}).fetch();
 
-				// var r = Math.floor(Math.random() * 5000);
-				// var randomVenue = Venues.find({},{ limit(1) , skip(r) });
+				// console.log('r:', r);
+				// console.log('randomUser:', randomUserId);
+				// console.log('randomUserId:', randomUserId[0]._id);
+				// console.log('randomUserName:', randomUserId[0].name);
+
+				var r = Math.floor(Math.random() * 5000);
+				var randomVenue = Venues.find({},{skip:r, limit:1}).fetch();
+				//var randomVenue = Venues.find({},{skip:10, limit:1});
+
+				startTime = getRandomIntInclusive(min, max);
+	      endTime = getRandomIntInclusive(startTime, max);
+
 
 	 			Events.insert({
 					eventName: words,
-					eventVenueName: randomeVenue.companyName,
-					eventVenueId: randomeVenue._id,
+				  //eventVenueName: randomVenue[0].companyName,
+				  eventVenueId: randomVenue[0]._id,
 					eventStreetAddress: streetAddress,
 					eventStreetName: streetName,
 					eventCity: city,
@@ -128,14 +146,14 @@ if (Events.find({}).count() === 0) {
 					eventZipCode: zipCode,
 					eventLat: latitude,
 					eventLon: longitude,
-					eventStartDate: "May 05",
-					eventEndDate: "May 05",
-					eventStartTime: "1:00pm",
-					eventEndTime: "4:00pm",
-					createdUser: randomUser,
+
+					eventStartDate: moment(startTime),
+					eventEndDate: moment(endTime),
+
+				  createdUser: randomUserId[0]._id,
 					eventDateCreated: now,
 					summary: paragraph,
-					eventNumberInAttendance: 5
+					eventNumberInAttendance: r
 	 			})
 	 });
 
@@ -281,3 +299,18 @@ if (Events.find({}).count() === 0) {
 }
 
 });
+
+function randomDate(start, end, startHour, endHour) {
+	var date = new Date(+start + Math.random() * (end - start));
+	var hour = startHour + Math.random() * (endHour - startHour) | 0;
+	date.setHours(hour);
+	return date;
+}
+
+function getRandomIntInclusive(min, max) {
+	// Returns a random integer between min (included) and max (included)
+	// Using Math.round() will give you a non-uniform distribution!
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
